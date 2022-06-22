332367ba40b1562e48c71494d4d17c3eac69e84c
95kreal <95k.real@gmail.com>
2022-06-22 14:28:16 +0300

    first commit

---
+++ src/test/java/ru/netology/manager/test/GameTest.java
@@ -0,0 +1,17 @@
+package ru.netology.manager.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Player;
+import ru.netology.exeption.NotRegisteredException;
+import ru.netology.manager.Game;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class GameTest {
+
+    
+}
